d392c90ec7ab08e3e2a13dec012b810bd970e506
Sami <oleg20011016@yandex.ru>
2023-03-01 23:44:38 +1000

    1.2

--- BookCourt/database/models.py
+++ BookCourt/database/models.py
@@ -7,9 +7,10 @@ class Books(models.Model):
     Description = models.TextField('Описание', max_length=10000)
     Price = models.IntegerField('Цена')
     Remainder = models.IntegerField('В наличии')
+    Image = models.CharField('Картинка', max_length=1000)
     NumberOfPages = models.IntegerField('Количество страниц')
     Genre = models.CharField('Жанр', max_length=100)
 
 
     def __str__(self):
-        return self.title
+        return self.Name
